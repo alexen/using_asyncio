c6636b78d8bfe0029aa5e969c5ffadd3e038c968
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2022-06-30 10:23:17 +0300

    Убрали return из корутины и использовали asyncio.wait() для запуска
    Программа ругается на невостребованный exception:
    
    Task exception was never retrieved
    future: <Task finished name='Task-3' coro=<bad_task() done, defined at use_wait.py:15> exception=RuntimeError('Moo-ha-ha-ha-ha!!! I am DrEvil!!!')>

--- use_wait.py
+++ use_wait.py
@@ -10,10 +10,6 @@ async def good_task(name, count, delay):
         print("[{}]: Task <{}> is doing work #{}/{}".format(time.strftime("%T"), name, n, count))
         await asyncio.sleep(delay)
     print("[{}]: Task <{}> is done".format(time.strftime("%T"), name))
-    return dict(
-        task_name = name,
-        status = 'OK'
-        )
 
 
 async def bad_task(name, count, delay):
@@ -27,16 +23,11 @@ async def main():
 
     print(">> START!")
 
-    try:
-        results = await asyncio.gather(
-            good_task("Alice", 5, 1),
-            good_task("Clark", 2, 3),
-            bad_task("DrEvil", 3, 1),
-            return_exceptions = True
-        )
-        print(f">> Results: {results}")
-    except RuntimeError as e:
-        print(f"Wow! Some task raised an exception: {e}")
+    await asyncio.wait([
+        good_task("Alice", 5, 1),
+        good_task("Clark", 2, 3),
+        bad_task("DrEvil", 3, 1)
+    ])
 
     print(">> FINISH (time: {})!".format(time.time() - start_time))
 
